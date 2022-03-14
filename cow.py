612609d3e17168b9db44521856a2ffc3458f0adc
Pink-Hat-Hacker <zyv@udel.edu>
2022-03-13 21:30:08 -0400

    not much progress

--- cow.py
+++ cow.py
@@ -2,6 +2,24 @@
 # CISC320 - SP2022
 # Cow! Algo Problem
 
-test_file = open("test.txt", "r+")
+'''
+test_file = open('test.txt', 'r')
+content = test_file.read()
+print(content)
+test_file.close()
+'''
 
-print(test_file.read())
+'''
+ok lets think about this:
+- the last element is the time stamp 
+--- we can use this are the sorting "key"
+--- use an integrated sorting algo to take the last 
+element of the string/list/dicitionary (figure this out later)
+and sort the file
+
+- then we need to check for a "M", "W", or "T"
+
+'''
+with open('test.txt') as f:
+    cows = dict(i.strip().split(None, 1) for i in f)
+print(cows)
